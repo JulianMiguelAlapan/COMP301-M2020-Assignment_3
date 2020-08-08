6fccacd6dbdb678c74df193e45c8cdc53850aff4
Julian Miguel Alapan <66970414+JulianMiguelAlapan@users.noreply.github.com>
2020-08-07 22:25:08 -0400

    Update copyfile.py

--- copyfile.py
+++ copyfile.py
@@ -2,3 +2,13 @@
 #Author:Julian Miguel Alapan
 #Description:Copy
 #Date:August 7, 2020
+
+import shutil
+
+filename=input("Enter the first filename")
+file1=open(filename1, 'rb')
+
+filename2=input("Enter the second filename")
+file2=open(filename2, 'wb')
+
+shutil.copyfileobj(file1, file2)
